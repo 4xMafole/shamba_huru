68be937e001ef6721b4fec354efeeaf4f785e5a6
4xmafole <4cruxt.unknown@gmail.com>
2022-03-14 12:42:39 -0700

    Added custom color class

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color paleGreen = Color(0xFF53AC69);
+  static Color deepGreen = Color(0xFF295A35);
+  static Color brownish = Color(0xFFA1887F);
+  static Color paleBlue = Color(0xff71A5D7);
+  static Color deepBlue = Color(0xFF1961A5);
+}
