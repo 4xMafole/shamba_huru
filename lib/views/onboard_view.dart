335e7073d464e7cf50f8e938f949ece24763e0d9
4xmafole <4cruxt.unknown@gmail.com>
2022-03-14 12:42:26 -0700

    Updated onboard view

--- lib/views/onboard_view.dart
+++ lib/views/onboard_view.dart
@@ -1,10 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_onboard/flutter_onboard.dart';
+import 'package:get/get.dart';
+import 'package:shamba_huru/utils/app_colors.dart';
+import 'package:shamba_huru/views/login_view.dart';
 
 class OnboardView extends StatelessWidget {
-  const OnboardView({Key? key}) : super(key: key);
+  OnboardView({Key? key}) : super(key: key);
+
+  final PageController _pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: OnBoard(
+        imageHeight: 200,
+        imageWidth: 200,
+        pageController: _pageController,
+        onBoardData: onBoardData,
+        titleStyles: TextStyle(
+          color: AppColor.deepGreen,
+          fontSize: 18,
+          fontWeight: FontWeight.w900,
+          letterSpacing: 0.15,
+        ),
+        descriptionStyles: TextStyle(
+          fontSize: 16,
+          color: AppColor.brownish,
+        ),
+        pageIndicatorStyle: PageIndicatorStyle(
+          width: 100,
+          inactiveColor: AppColor.paleGreen,
+          activeColor: AppColor.deepGreen,
+          inactiveSize: const Size(8, 8),
+          activeSize: const Size(12, 12),
+        ),
+        // Either Provide onSkip Callback or skipButton Widget to handle skip state
+        skipButton: TextButton(
+          onPressed: () {
+            Get.to(const LoginView());
+          },
+          child: Text(
+            "Skip",
+            style: TextStyle(color: AppColor.paleGreen),
+          ),
+        ),
+        // Either Provide onDone Callback or nextButton Widget to handle done state
+        nextButton: OnBoardConsumer(
+          builder: (context, ref, child) {
+            final state = ref.watch(onBoardStateProvider);
+            return InkWell(
+              onTap: () => _onNextTap(state),
+              child: Container(
+                width: 230,
+                height: 50,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  gradient: LinearGradient(
+                    colors: [AppColor.deepGreen, AppColor.paleGreen],
+                  ),
+                ),
+                child: Text(
+                  state.isLastPage ? "Done" : "Next",
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  void _onNextTap(OnBoardState onBoardState) {
+    if (!onBoardState.isLastPage) {
+      _pageController.animateToPage(
+        onBoardState.page + 1,
+        duration: const Duration(milliseconds: 250),
+        curve: Curves.easeInOutSine,
+      );
+    } else {
+      Get.to(const LoginView());
+    }
   }
 }
+
+final List<OnBoardModel> onBoardData = [
+  const OnBoardModel(
+    title: "Set your own goals and get better",
+    description: "Goal support your motivation and inspire you to work harder",
+    imgUrl: "assets/images/tractor.png",
+  ),
+  const OnBoardModel(
+    title: "Track your progress with statistics",
+    description:
+        "Analyse personal result with detailed chart and numerical values",
+    imgUrl: 'assets/images/farm.png',
+  ),
+  const OnBoardModel(
+    title: "Create photo comparision and share your results",
+    description:
+        "Take before and after photos to visualize progress and get the shape that you dream about",
+    imgUrl: 'assets/images/cow.png',
+  ),
+];
